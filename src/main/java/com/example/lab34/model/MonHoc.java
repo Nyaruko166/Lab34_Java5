5fb4ac6fb5a9e40a16431adcb8edc93bc3a73e78
Nyaruko <occho1666@gmail.com>
2023-05-18 21:57:04 +0700

    MonHoc Class

---
+++ src/main/java/com/example/lab34/model/MonHoc.java
@@ -0,0 +1,23 @@
+package com.example.lab34.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MonHoc {
+    
+    private String maMon;
+
+    private String tenMon;
+
+    private Integer soTinChi;
+
+    private String chuyenNganh;
+
+    private String[] giangVien;
+
+    private Boolean batBuoc;
+}
